822957d0a9d9e4e6d115e5597936a3942b7dee7b
Bolotskikh Mark <markbolotskih@ya.ru>
2020-12-27 00:51:05 +0300

    Поставил мощную защиту от дурака, вынес валидацию и калькуляцию в отдельные классы, покрыл тестами.

---
+++ Calculator.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CalculatorConsoleApp
+{
+    public class Calculator
+    {
+        private static string pattern = @"-?\d+(?:\,\d+)?";
+
+        public Calculator(string pattern)
+        {
+            Calculator.pattern = pattern ?? Calculator.pattern;
+        }
+        /// <summary>
+        /// Метод принимает выражение в виде строки.
+        /// Алгоритм обработки:
+        /// 1) Разбирается с содержимым всех скобок;
+        /// 2) Производит все умножения и деления в том порядке, в котором они идут в выражении;
+        /// 3) Разбивает оставшееся выражение на операнды, и складывает их все.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string Calc(string input)
+        {
+            HandleAllBrackets(ref input);
+            HandleAllMultiplyAndDivide(ref input);
+            HandleAllSumsAndSubtractions(ref input);
+
+            return input;
+        }
+        private static double GetLeftOperand(string input, int index)
+        {
+            string leftpartofinput = input.Substring(0, index);
+
+            var alloperands = Regex.Matches(leftpartofinput, pattern);
+
+            var numberofoperands = alloperands.Count;
+
+            double operand = Convert.ToDouble(alloperands[numberofoperands - 1].Value, CultureInfo.CurrentCulture);
+
+            return operand;
+        }
+        private static double GetRightOperand(string input, int index)
+        {
+            string rightpartofinput = input.Substring(index + 1);
+
+            var alloperands = Regex.Matches(rightpartofinput, pattern);
+
+            double operand = Convert.ToDouble(alloperands[0].Value, CultureInfo.CurrentCulture);
+
+            return operand;
+        }
+        private static string GetWhatIsBetweenBrackets(string input, int index)
+        {
+            string str = "";
+
+            int numberofbrackets = 1;
+
+            for (int j = index + 1; j < input.Length; j++)
+            {
+                if (input[j] == '(')
+                {
+                    numberofbrackets++;
+                }
+                else if (input[j] == ')')
+                {
+                    numberofbrackets--;
+                }
+
+                if (numberofbrackets == 0)
+                    break;
+
+                str += input[j];
+            }
+
+            return str;
+        }
+        private static void HandleAllMultiplyAndDivide(ref string input)
+        {
+            int asterixIndex, slashIndex;
+
+            while (input.Contains('*') || input.Contains('/'))
+            {
+                asterixIndex = input.IndexOf('*');
+                slashIndex = input.IndexOf('/');
+                if (asterixIndex == -1)
+                {
+                    Divide(ref input, slashIndex);
+                }
+                else if (slashIndex == -1 || asterixIndex < slashIndex)
+                {
+                    Multiply(ref input, asterixIndex);
+                }
+                else
+                {
+                    Divide(ref input, slashIndex);
+                }
+            }
+        }
+        public static void Multiply(ref string input, int index)
+        {
+            double leftoperand = GetLeftOperand(input, index);
+            double rightoperand = GetRightOperand(input, index);
+
+            string toreplace = $"{leftoperand}*{rightoperand}";
+            string toreplacewith = (leftoperand * rightoperand).ToString();
+            input = input.Replace(toreplace, toreplacewith);
+        }
+        public static void Divide(ref string input, int index)
+        {
+            double leftoperand = GetLeftOperand(input, index);
+            double rightoperand = GetRightOperand(input, index);
+
+            string toreplace = $"{leftoperand}/{rightoperand}";
+            string toreplacewith = (leftoperand / rightoperand).ToString();
+
+            input = input.Replace(toreplace, toreplacewith);
+        }
+        /// <summary>
+        /// Пока в выражении есть скобки, метод находит и обрабатывает методом Calc их содержимое
+        /// </summary>
+        /// <param name="input"></param>
+        private static void HandleAllBrackets(ref string input)
+        {
+            while (input.Contains('('))
+            {
+                int index = input.LastIndexOf('(');
+
+                string toreplace = GetWhatIsBetweenBrackets(input, index);
+
+                string toreplacewith = Calc(toreplace);
+
+                input = input.Replace($"({toreplace})", toreplacewith);
+            }
+
+            input = input.Replace("--", "+");
+        }
+        /// <summary>
+        /// Метод разбивает выражение на операнды и заменяет выражение их суммой
+        /// </summary>
+        /// <param name="input"></param>
+        private static void HandleAllSumsAndSubtractions(ref string input)
+        {
+            input = input.Trim('+');
+
+            var alloperands = Regex.Matches(input, pattern);
+            if (alloperands.Count > 1)
+            {
+                double sum = 0;
+                for (int i = 0; i < alloperands.Count; i++)
+                {
+                    double operand = Convert.ToDouble(alloperands[i].Value, CultureInfo.CurrentCulture);
+                    sum += operand;
+                }
+
+                input = sum.ToString();
+            }
+        }
+
+
+    }
+}
---
+++ Validator.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+
+namespace CalculatorConsoleApp
+{
+    public class Validator
+    {
+        private string pattern = @"-?\d+(?:\,\d+)?";
+        private string ErrorMessage;
+        private List<string> unallowedPairings = new List<string>() { "/*", "*/", "/+", "+/", "+*", "*+" };
+        private Action<string> method;
+
+        public Validator(Action<string> method)
+        {
+            this.method = method;
+        }
+        public Validator(string newPattern, Action<string> method, List<string> newUnallowedPairings)
+        {
+            this.method = method;
+            this.pattern = newPattern ?? this.pattern;
+            unallowedPairings = newUnallowedPairings;
+        }
+        public bool IsValid(string input)
+        {
+            return ContainsOnlyAllowedSymbols(input) && ContainsRightAmountOfBrackets(input) && !ContainsExtraComma(input) && !ContainsUnallowedPairings(input);
+        }
+        private bool ContainsRightAmountOfBrackets(string input)
+        {
+            int countOfOpeningBrackets = input.Count(x => x == '(');
+            int countOfClosingBrackets = input.Count(x => x == ')');
+
+            if (!(countOfOpeningBrackets == countOfClosingBrackets))
+            {
+                ErrorMessage = "Открывающих и закрывающих скобок должно быть одинаковое количество";
+                return false;
+            }
+            return true;
+        }
+        private bool ContainsOnlyAllowedSymbols(string input)
+        {
+
+            char current;
+            List<char> allowedSymbols = new List<char>() { '(', ')', '*', '/', '+', '-', ',' };
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                current = input[i];
+
+                if (char.IsDigit(current))
+                    continue;
+                if (char.IsLetter(current))
+                {
+                    ErrorMessage = "Строка не должна содержать букв";
+                    return false;
+                }
+                if (!allowedSymbols.Contains(current))
+                {
+                    ErrorMessage = "Строка содержит символы, которых быть не должно";
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        private bool ContainsExtraComma(string input)
+        {
+            var alloperands = Regex.Matches(input, pattern);
+
+            foreach (var operand in alloperands)
+            {
+                input = input.Replace(operand.ToString(), "");
+            }
+
+            if (input.Contains(','))
+            {
+                ErrorMessage = "Кажется, у вас были лишние запятые";
+                return true;
+            }
+
+            return false;
+        }
+        private bool ContainsUnallowedPairings(string input)
+        {
+            foreach (var pairing in unallowedPairings)
+            {
+                if (input.Contains(pairing))
+                {
+                    ErrorMessage = "Выражение содержит недопустимые символы";
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public void ShowError()
+        {
+            method?.Invoke(ErrorMessage);
+        }
+        public void FixInput(ref string input)
+        {
+            input = input.Trim();
+
+            input = input.Replace('.', ',');
+
+            input = input.Replace("--", "+");
+
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i != 0 && input[i] == '(')
+                {
+                    if (char.IsDigit(input[i - 1]))
+                    {
+                        input = input.Insert(i, "*");
+                    }
+                }
+            }
+            input = input.Replace("()", "0");
+        }
+    }
+}
